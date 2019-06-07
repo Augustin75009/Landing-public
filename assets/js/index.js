241d54011e0d4d87a5a2d9d7e85750881259eb8c
augustin cousin <augustincousin.pro@gmail.com>
2019-06-07 21:21:21 +0200

    v1 Pico

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,7 @@
 const header = document.querySelector("#null")
+const dropdown = document.querySelectorAll(".tab")
+const dropdown1 = document.querySelector(".dropdown-content1 div")
+const dropdown0 = document.querySelector(".dropdown-content0 div")
 
 document.addEventListener("scroll", (event) => {
   if (window.pageYOffset > 10) {
@@ -6,4 +9,17 @@ document.addEventListener("scroll", (event) => {
   } else {
     header.classList.remove("border")
   }
+});
+
+
+dropdown.forEach((d, index) => {
+  d.addEventListener("mouseover", (event) => {
+    let target = event.currentTarget.childNodes[3]
+    target.classList.add("active")
+
+  })
+  d.addEventListener("mouseout", (event) => {
+    let target = event.currentTarget.childNodes[3]
+    target.classList.remove("active")
+  })
 });
